149086978c235f1f19dcb6a6ee523f6a9e527c1d
Michael Stephens <mdstephens26@yahoo.com>
2020-12-06 23:37:34 -0500

    commit tree c5e50e6aa4d4e0c5f0011c1a85ad85d882a60736

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+require 'pry'
+class Song
+    attr_accessor :name, :artist, :genre
+    @@artists = []
+    @@genres = []
+    @@count = 0
+    def initialize(name, artist, genre)
+      @@count += 1
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@artists << @artist
+      @@genres << @genre
+    end
+    def self.count
+      @@count
+    end
+    def self.artists
+        @@artists.uniq
+    end
+    def self.genres
+        @@genres.uniq
+    end
+    def self.genre_count
+        genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre] #exists
+        genre_count[genre] += 1
+      else #none
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+    end
+    def self.artist_count
+        artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] #exists
+        artist_count[artist] += 1
+      else #none
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+    end
+
+
+  end
